a40a979d107c6b3be6a308abb3f897373f884d61
Blizonaire <Necesku@gmail.com>
2023-12-08 19:50:34 +0100

    Some dashboard enchancements

---
+++ src/http/features/welcomer.ts
@@ -0,0 +1,26 @@
+import { Response } from "express";
+import { prisma } from "../../main";
+
+export async function save(guild_id: string, data: any, res: Response) {
+    try {
+        await prisma.guildConfiguration.update({
+            where: {
+                id: guild_id,
+            },
+            data: {
+                welcomemsg: data.welcome_message,
+                welcomechannel: data.welcome_channel,
+                leavemsg: data.leave_message,
+                leavechannel: data.leave_channel,
+            },
+        });
+        res.json({
+            message: "Success!",
+        });
+    } catch (e) {
+        res.status(500).json({
+            error: true,
+            message: "An error occured while trying to save!",
+        });
+    }
+}
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,10 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import { bot, prisma } from "../main.js";
 import * as logger from "../utils/logger.js";
 import { cnfCache, hasAdministrator } from "../utils/connector_utils.js";
+import fs from "fs";
+import { dirname, join } from "path";
 
 const app = express();
 
@@ -37,27 +39,23 @@ app.use(bodyParser.json());
 
 const port = 443;
 
+app.get("/teapot", async (req, res) => {
+    res.status(418).json({ "message": "I don't serve coffee, because I'm a teapot! 🫖" })
+})
+
 app.get("/api/get_channels", async (req, res) => {
     const jData = req.query;
     const authorization = req.headers.authorization;
     if (jData.guild_id && authorization) {
-        try {
-            cache = await cnfCache(cache, authorization.toString());
-        } catch (e) {
-            res.status(500).json({
-                error: true,
-                rtm: true,
-                message: "Something unexpected happened!",
-            });
-        }
+        cache = await cnfCache(cache, authorization.toString());
         const id = cache[authorization.toString()];
         if (id) {
-            if (!hasAdministrator(bot, jData.guild_id.toString(), id))
+            if (!hasAdministrator(bot, jData.guild_id.toString(), id)) {
                 return res.status(403).json({
                     error: true,
-                    rtm: true,
                     message: "You don't have Administrator permission!",
                 });
+            }
             const guild = bot.guilds.cache.get(jData.guild_id.toString());
             if (guild) {
                 let channels: any = [];
@@ -75,20 +73,17 @@ app.get("/api/get_channels", async (req, res) => {
                 } else {
                     res.status(500).json({
                         error: true,
-                        rtm: true,
                         message: "The bot is not added to that guild!",
                     });
                 }
             } else
                 res.status(500).json({
                     error: true,
-                    rtm: true,
                     message: "The bot is not added to that guild!",
                 });
         } else
             res.status(500).json({
                 error: true,
-                rtm: true,
                 message: "Something unexpected happened!",
             });
     } else
@@ -100,90 +95,48 @@ app.get("/api/get_guilds", async (req, res) => {
     const authorization = req.headers.authorization;
     gids = gids?.toString().split(",");
     if (authorization && gids) {
-        try {
-            cache = await cnfCache(cache, authorization.toString());
-        } catch (e) {
-            res.status(500).json({
-                error: true,
-                rtm: true,
-                message: "Something unexpected happened!",
-            });
-        }
-        try {
-            let guilds: any = [];
-            gids.forEach((gid: any) => {
-                const guild = bot.guilds.cache.get(gid);
-                if (guild)
-                    guilds.push({
-                        id: guild?.id,
-                        name: guild?.name,
-                    });
-            });
-            res.json({
-                guilds: guilds,
-            });
-        } catch (error) {
-            return res.status(500).json({
-                error: true,
-                rtm: true,
-                message: "Something went wrong, try again later!",
-            });
-        }
+        cache = await cnfCache(cache, authorization.toString());
+        let guilds: any = [];
+        gids.forEach((gid: any) => {
+            const guild = bot.guilds.cache.get(gid);
+            if (guild)
+                guilds.push({
+                    id: guild?.id,
+                    name: guild?.name,
+                });
+        });
+        res.json({
+            guilds: guilds,
+        });
     } else {
-        return res.status(400).json();
+        return res.status(400);
     }
 });
 
-app.post("/api/save", async (req, res) => {
+app.post("/api/save", async (req: Request, res: Response) => {
     let { feature, guild_id, data } = req.body;
     const authorization = req.headers.authorization;
     if (feature && authorization && data && guild_id) {
-        try {
-            cache = await cnfCache(cache, authorization.toString());
-        } catch (e) {
-            res.status(500).json({
-                error: true,
-                rtm: true,
-                message: "Something unexpected happened!",
-            });
-        }
+        cache = await cnfCache(cache, authorization.toString());
         const id = cache[authorization.toString()];
         if (id) {
-            if (!hasAdministrator(bot, guild_id.toString(), id))
+            if (!hasAdministrator(bot, guild_id.toString(), id)) {
                 return res.status(403).json({
                     error: true,
-                    rtm: true,
                     message: "You don't have Administrator permission!",
                 });
-            if (feature == "welcomer") {
-                try {
-                    await prisma.guildConfiguration.update({
-                        where: {
-                            id: guild_id,
-                        },
-                        data: {
-                            welcomemsg: data.welcome_message,
-                            welcomechannel: data.welcome_channel,
-                            leavemsg: data.leave_message,
-                            leavechannel: data.leave_channel,
-                        },
-                    });
-                    res.json({
-                        message: "Success!",
-                    });
-                } catch (e) {
-                    logger.error(e);
-                    res.status(500).json({
-                        error: true,
-                        rtm: true,
-                        message: "An error occured while trying to save!",
-                    });
-                }
+            }
+            if (fs.existsSync(join(dirname(import.meta.url), "features", feature + ".ts"))) {
+                await import(`./features/${feature + ".ts"}`);
+            } else {
+                res.status(400).json({
+                    error: true,
+                    message: "Feature not implemented.",
+                });
             }
         } else {
             res.status(500).json({
                 error: true,
-                rtm: true,
                 message: "Something unexpected happened!",
             });
         }
